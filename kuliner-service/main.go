b5593ee5dc161c0cae1d127c32566b3d289cdb30
Andi Bahdar <andibahdar@Mac-Deceng.local>
2026-09-17 15:21:35 +0700

    perubahan terakhir

--- kuliner-service/main.go
+++ kuliner-service/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
